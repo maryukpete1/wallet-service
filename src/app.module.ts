5b4fe16edd4072103190d6e79cf82f23e4351d51
mary <maryukpete9@gmail.com>
2025-12-09 23:40:23 +0100

    chore: initial project setup

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import configuration from './config/configuration';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ApiKeysModule } from './api-keys/api-keys.module';
+import { WalletModule } from './wallet/wallet.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+    ApiKeysModule,
+    WalletModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
