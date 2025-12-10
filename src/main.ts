b49ce04d7f32f446797465a79c86f7e39bfc6b21
mary <maryukpete9@gmail.com>
2025-12-10 09:26:49 +0100

    fix: enable CORS for swagger ui

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Wallet Service API')
     .setDescription('API documentation for the Wallet Service')
